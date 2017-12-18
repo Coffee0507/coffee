9343ed1303fdf33308a88f953138ba809388a21f
Takahashiyuuri <a33a070e@gmail.com>
2017-12-18 21:58:58 +0900

    コミットします

--- workspace/practice/src/com/internousdev/practice/Action/LoginAction.java
+++ workspace/practice/src/com/internousdev/practice/Action/LoginAction.java
@@ -4,28 +4,34 @@ import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
+import com.internousdev.practice.DAO.BuyItemDAO;
 import com.internousdev.practice.DAO.LoginDAO;
+import com.internousdev.practice.DTO.BuyItemDTO;
 import com.internousdev.practice.DTO.LoginDTO;
 import com.opensymphony.xwork2.ActionSupport;
+
 
 public class LoginAction extends ActionSupport implements SessionAware{
 	private String loginuserId;
 	private String loginuserPass;
 	public Map<String,Object>session;
+	private BuyItemDAO buyitemDAO = new BuyItemDAO();
 	private LoginDAO loginDAO = new LoginDAO();
 	private LoginDTO loginDTO = new LoginDTO();
 	public String result;
-
 	public String execute(){
 		result = ERROR;
-
 		loginDTO = loginDAO.getLoginUserInfo(loginuserId, loginuserPass);
 		session.put("loginUser", loginDTO);
-
 		if(((LoginDTO)session.get("loginUser")).getLoginFlg()){
 			result = SUCCESS;
 
+			BuyItemDTO buyitemDTO = buyitemDAO.getBuyItemInfo();
 			session.put("login_user_id",loginDTO.getLoginId());
+			session.put("id",buyitemDTO.getId());
+			session.put("buyItem_name",buyitemDTO.getItemName());
+			session.put("buyItem_price",buyitemDTO.getitemPrice());
+
 
 			return result;
 		}
@@ -43,6 +49,7 @@ public class LoginAction extends ActionSupport implements SessionAware{
 	public void setLoginUserPass(String loginuserPass){
 		this.loginuserPass = loginuserPass;
 	}
+	@Override
 	public void setSession(Map<String,Object>session){
 		this.session = session;
 	}
