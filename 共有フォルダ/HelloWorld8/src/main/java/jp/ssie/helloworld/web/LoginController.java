c758534627954f47ab9ff550f228f524d4621a28
Coffee0507 <git.coffee.0507@gmail.com>
2018-03-05 00:13:55 +0900

    3/5:0:13

--- 共有フォルダ/HelloWorld8/src/main/java/jp/ssie/helloworld/web/LoginController.java
+++ 共有フォルダ/HelloWorld8/src/main/java/jp/ssie/helloworld/web/LoginController.java
@@ -1,5 +1,8 @@
 package jp.ssie.helloworld.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,27 +12,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import jp.ssie.helloworld.form.LoginForm;
+import jp.ssie.helloworld.model.User;
+import jp.ssie.helloworld.repository.UserRepository;
 import jp.ssie.helloworld.validation.GroupOrder;
 
 @Controller
 public class LoginController {
-
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String index(Model model) {
-		return "index";
-	}
+	@Autowired
+	private UserRepository userRep;
 
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String login(Model model, @Validated(GroupOrder.class) @ModelAttribute("loginForm") LoginForm loginForm,
 			BindingResult result) {
-
 		if (result.hasErrors()) {
 			return "index";
 		}
 
-		model.addAttribute("loginName", loginForm.getLoginName());
 
-		return "top";
+		List<User> userList = userRep.findAll();
 
+		for (User user : userList) {
+			if (user.getName().equals(loginForm.getLoginName())) {
+				model.addAttribute("loginName", loginForm.getLoginName());
+				return "top";
+			}
+		}
+		return "index";
 	}
 }
